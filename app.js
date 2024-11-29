38536ee85b57bf734c5c687d0eeb1298417020ac
hari-pra <157770405+hari-pra@users.noreply.github.com>
2024-11-29 19:47:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,20 +9,12 @@ app.use(bodyparser.urlencoded({extended:true}));
 const router = require('./routes/route');
 app.use('/',router);
 
-const connectToDb = async()=> {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/civilloan',{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        });
-        console.log('connected to db successfully');
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-};
-
-connectToDb();
+mongoose.connect('mongodb://127.0.0.1:27017/civilloan', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error(err));
 
 const port = 3010;
 app.listen(port,()=>{
